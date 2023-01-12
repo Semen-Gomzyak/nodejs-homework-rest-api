7cacdb69b345c271eb4527b207b220885a59dbe3
Homziak Semen <91370041+Semen-Gomzyak@users.noreply.github.com>
2023-01-12 20:49:17 +0100

    Revert "hm-03 done"

--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -4,29 +4,27 @@ const {
   getContactById,
   addContact,
   removeContact,
-  updateStatusContact,
-} = require('../../controllers/contacts.controller');
+  updateContact,
+} = require('../../models/contacts');
 const router = express.Router();
 
 router.get('/', async (req, res, next) => {
   try {
-    res.status(200).json(await listContacts(req.limit));
+    res.status(200).json(await listContacts());
   } catch (error) {
-    next(error);
+    next(error)
   }
 });
 
 router.get('/:contactId', async (req, res, next) => {
   try {
     const searchedContact = await getContactById(req.params.contactId);
-    if (!searchedContact) {
-      res
-        .status(404)
-        .send(`Contact with id ${req.params.contactId} can't be found`);
+    if(!searchedContact){
+      res.status(404).send(`Contact with id ${req.params.contactId} can't be found`);
     }
-    res.status(200).json(searchedContact);
+      res.status(200).json(searchedContact)         
   } catch (error) {
-    next(error);
+    next(error)
   }
 });
 
@@ -34,7 +32,7 @@ router.post('/', async (req, res, next) => {
   try {
     res.status(201).json(await addContact(req.body));
   } catch (error) {
-    next(error);
+    next(error)
   }
 });
 
@@ -50,24 +48,20 @@ router.delete('/:contactId', async (req, res, next) => {
     await removeContact(contactById);
     res.status(200).json({ message: 'contact deleted' });
   } catch (error) {
-    next(error);
+    next(error)
   }
 });
 
-router.put('/:contactId/favorite', async (req, res, next) => {
+router.put('/:contactId', async (req, res, next) => {
   try {
     const contactById = req.params.contactId;
-    const contactBody = req.body;
     const searchedContact = await getContactById(contactById);
-    if (!searchedContact) {
+    if(!searchedContact){
       res.status(404).send(`Contact with id ${contactById} can't be found`);
     }
-    if (!contactBody) {
-      res.status(404).send({ message: 'missing field favorite' });
-    }
-    res.status(200).json(await updateStatusContact(contactById, req.body));
+    res.status(200).json(await updateContact(contactById, req.body))
   } catch (error) {
-    next({ message: 'Not found' }, error);
+    next(error)
   }
 });
 
--- server.js
+++ server.js
@@ -1,23 +1,5 @@
-const app = require('./app');
-const mongoose = require('mongoose');
-const dotenv = require('dotenv');
+const app = require('./app')
 
-dotenv.config();
-mongoose.set('strictQuery', false);
-
-const { HOST_URI } = process.env;
-
-async function main() {
-  try {
-    await mongoose.connect(HOST_URI);
-    console.log('Database connection successful');
-
-    app.listen(3000, () => {
-      console.log('Server running. Use our API on port: 3000');
-    });
-  } catch (error) {
-    console.error('main failed', error.message);
-  }
-}
-
-main();
+app.listen(3000, () => {
+  console.log("Server running. Use our API on port: 3000")
+})
